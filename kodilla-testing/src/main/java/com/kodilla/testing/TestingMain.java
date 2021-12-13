9baa5052927429bcac260b136c8c88c6ec2b99dd
Adrian Zienkiewicz <survivalarm@gmail.com>
2021-12-13 21:28:01 +0100

    6.2 Added kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,42 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator cal = new Calculator();
+        int resultAdd;
+        int resultSub;
+
+        resultAdd = cal.add(5,5);
+
+        if (resultAdd == (5+5)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        resultSub = cal.sub(99,11);
+
+        if (resultSub == 99-11) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
     }
 }
