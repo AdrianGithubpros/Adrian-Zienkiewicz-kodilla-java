6b0d2ef7d54fbcbfeb3076a60484a86319d748c3
Adrian Zienkiewicz <survivalarm@gmail.com>
2022-01-02 17:52:31 +0100

    Task 7.2 task

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -45,6 +46,7 @@ public class StreamMain {
         poemBeautifier.beautify("third",(text)-> "ABC" + text + "ABC");
         poemBeautifier.beautify("fourth",(text)-> text.replace('u','U'));
 
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
