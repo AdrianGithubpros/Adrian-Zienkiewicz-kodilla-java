2a8f99d7f03df4eaf10f1f285670f0769540c62c
Adrian Zienkiewicz <survivalarm@gmail.com>
2022-01-02 17:18:55 +0100

    Task 7.1 task

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.Locale;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -24,5 +25,26 @@ public class StreamMain {
             System.out.println(c); // sprawdzam dla zabawy
 
         });
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("first",(text)-> text.toUpperCase());
+        poemBeautifier.beautify("SeCoNd",(text)-> text.toLowerCase());
+        poemBeautifier.beautify("third",(text)-> "ABC" + text + "ABC");
+        poemBeautifier.beautify("fourth",(text)-> text.replace('u','U'));
+
+
     }
 }
