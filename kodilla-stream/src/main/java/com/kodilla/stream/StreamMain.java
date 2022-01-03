a3d083b7739b1fa96720325fcaed0c1f45bc5700
Adrian Zienkiewicz <survivalarm@gmail.com>
2022-01-03 17:15:07 +0100

    Task 7.4.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,16 +4,17 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.util.Calendar;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-
+        Calendar calendar = Calendar.getInstance();
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMap = theForum.getList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getDate().getYear() < 2002)
+                .filter(forumUser -> (calendar.get(Calendar.YEAR) - forumUser.getDate().getYear()) > 20)
                 .filter(forumUser -> forumUser.getNumberOfPosts() > 0)
                 .collect(Collectors.toMap(ForumUser::getId, forumUser -> forumUser));
 
