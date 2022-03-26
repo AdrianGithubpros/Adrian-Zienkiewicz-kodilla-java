60a8eda20b953d31d8483b53a05cff79b1282347
Adrian Zienkiewicz <survivalarm@gmail.com>
2022-03-26 19:37:11 +0100

    Task 17.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.dao.TaskDao;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "task for task";
+
+
+
+    @Test
+    void testFindByListName(){
+        //Given
+        TaskList taskList = new TaskList( "Oname-Wa", DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTasksList = taskListDao.findByListName(listName);
+
+        //Then
+        assertEquals(1, readTasksList.size());
+
+        //CleanUp
+       // int id = readTasksList.get(0).getId();
+        //taskListDao.deleteById(id);
+    }
+}
