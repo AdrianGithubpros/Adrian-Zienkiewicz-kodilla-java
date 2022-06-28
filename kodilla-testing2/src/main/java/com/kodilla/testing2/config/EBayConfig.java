a0636bc3e7d7d52b91741409f4e1cea65eda40ec
Adrian Zienkiewicz <survivalarm@gmail.com>
2022-06-28 22:51:25 +0200

    31.1.1

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/EBayConfig.java
@@ -0,0 +1,7 @@
+package com.kodilla.testing2.config;
+
+public class EBayConfig {
+    public static final String SEARCHFIELD = "_nkw";
+    public static final String DECLINECOOKIES = "gdpr-banner-decline";
+    public static final String GDPRBANNER = "gdpr-banner";
+}
